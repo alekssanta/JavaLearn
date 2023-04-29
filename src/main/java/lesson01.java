e47f30cf4106c5818345bbad154a41dda73c7bf0
alekssanta <WBKmnBn348Gxng2>
2023-04-29 19:08:08 +0400

    lesson 01 дз - 2 done

--- src/main/java/lesson01.java
+++ src/main/java/lesson01.java
@@ -47,7 +47,33 @@ public class lesson01 {
         //1. Задать целочисленный массив, состоящий из элементов 0 и 1. Например: [ 1, 1, 0, 0, 1, 0, 1, 1, 0, 0 ]. С помощью цикла и условия заменить 0 на 1, 1 на 0;
         int[] arr = {1, 1, 0, 0, 1, 0, 1, 1, 0, 0};
         invert(arr);
-        System.out.println(Arrays.toString(arr)); //Вывод: [0, 0, 1, 1, 0, 1, 0, 0, 1, 1]
+        System.out.println(Arrays.toString(arr));
+        //Задать пустой целочисленный массив размером 8. С помощью цикла заполнить его значениями 0 3 6 9 12 15 18 21;
+
+        int size = 8;
+        int[] array = new int[size];
+        for (int j = 0; j < size; j++) {
+            array[j] = j * 3;
+        }
+        System.out.println(Arrays.toString(array)); //Вывод: [0, 3, 6, 9, 12, 15, 18, 21]
+
+        //3. Задать массив [ 1, 5, 3, 2, 11, 4, 5, 2, 4, 8, 9, 1 ] пройти по нему циклом, и числа меньшие 6 умножить на 2;
+
+        int[] array3 = {1,5,3,2,11,4,5,2,4,8,9,1};
+        for (int k = 0; k < array3.length; k++) {
+            if (array3[k] < 6) {
+                array3[k] *= 2;
+            }
+        }
+        System.out.println(Arrays.toString(array3));
+        //4. Создать квадратный двумерный целочисленный массив (количество строк и столбцов одинаковое), и с помощью цикла(-ов) заполнить его диагональные элементы единицами;
+        int[][] array4 = new int[5][5];
+        for (int l = 0; l < array4.length; l++) {
+
+        }
+        for (int l = 0; l < array4.length; l++) {
+            System.out.println(Arrays.toString(array4[l]));
+        }
     }
 
     private static double calculateExpression(double a, double b, double c, double d) {
@@ -86,6 +112,7 @@ public class lesson01 {
             }
         }
     }
+
     }
 
 
